39e213f3aa2af5bbc1eb4a1fd05373c5451f2a82
Matthew Higgins <developer@matthewhiggins.com>
2012-08-02 20:33:16 -0700

    add test when using options

--- test/arelastic/builders/filter_test.rb
+++ test/arelastic/builders/filter_test.rb
@@ -52,6 +52,9 @@ class Arelastic::Builders::FilterTest < MiniTest::Spec
   def test_distance
     expected = {"geo_distance" => {"distance" => "10km", "color" => [10, 11]}}
     assert_equal expected, builder.distance('10km', [10, 11]).as_elastic
+
+    expected = {"geo_distance" => {"distance" => "10km", "color" => [10, 11], "distance_type" => "plane"}}
+    assert_equal expected, builder.distance('10km', [10, 11], 'distance_type' => 'plane').as_elastic
   end
 
   private
