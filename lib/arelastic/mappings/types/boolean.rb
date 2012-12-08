f7fb2232db0e0e425c591d91c96368ff7389bb3e
Matthew Higgins <developer@matthewhiggins.com>
2012-12-07 19:07:47 -0800

    move types to mapping/types

--- lib/arelastic/mappings/types/boolean.rb
+++ lib/arelastic/mappings/types/boolean.rb
@@ -1,6 +1,6 @@
 module Arelastic
   module Mappings
-    class Boolean < Arelastic::Mappings::Mapping
+    class Boolean < Arelastic::Mappings::Type
       for_type 'boolean'
     end
   end
--- lib/arelastic/mappings/types/number.rb
+++ lib/arelastic/mappings/types/number.rb
@@ -1,6 +1,6 @@
 module Arelastic
   module Mappings
-    class Number < Arelastic::Mappings::Mapping
+    class Number < Arelastic::Mappings::Type
       def initialize(type, options = {})
       end
     end
