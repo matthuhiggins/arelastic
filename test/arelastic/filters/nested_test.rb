949c419da97c2f377eb3ad95d679899a5b62eef4
Matthew Higgins <developer@matthewhiggins.com>
2013-09-19 17:47:20 -0700

    Fix the build

--- test/arelastic/filters/nested_test.rb
+++ test/arelastic/filters/nested_test.rb
@@ -13,7 +13,7 @@ class Arelastic::Filters::NestedTest < MiniTest::Unit::TestCase
       }
     }
 
-    assert_equal expected, Arelastic::Filters::Nested.new('bunnies', Arelastic::Searches::Query(Arelastic::Queries::Match.new('name', 'Harry'))).as_elastic
+    assert_equal expected, Arelastic::Filters::Nested.new('bunnies', Arelastic::Searches::Query.new(Arelastic::Queries::Match.new('name', 'Harry'))).as_elastic
   end
 
 end
