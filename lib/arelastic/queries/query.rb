da95578c47fa787402714304fdd2ab34b0bb7b93
Matthew Higgins <developer@matthewhiggins.com>
2015-09-23 22:32:51 -0700

    Add queries/nested

--- lib/arelastic/queries/query.rb
+++ lib/arelastic/queries/query.rb
@@ -1,6 +1,9 @@
 module Arelastic
   module Queries
     class Query < Arelastic::Nodes::Node
+      def nested path
+        Arelastic::Queries::Nested.new path, "query" => self.as_elastic
+      end
     end
   end
 end
--- test/arelastic/queries/query_test.rb
+++ test/arelastic/queries/query_test.rb
@@ -1,6 +1,18 @@
 require 'helper'
 
 class Arelastic::Queries::QueryTest < Minitest::Test
-  def test_as_elastic
+  def test_nested
+    query = Arelastic::Queries::Term.new('foo', 'bar')
+
+    nested_query = query.nested 'links'
+
+    assert nested_query.is_a?(Arelastic::Queries::Nested)
+    expected = {
+      "nested" => {
+        "path" => "links",
+        "query" => query.as_elastic
+      }
+    }
+    assert_equal(expected, nested_query.as_elastic)
   end
 end
