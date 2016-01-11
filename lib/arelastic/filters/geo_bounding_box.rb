e6dac0878483672f23b892b5a03baf43d4aa6e68
Matthew Higgins <developer@matthewhiggins.com>
2016-01-10 20:15:31 -0800

    Making the bad decision to use ordered arguments rather than named parameters

--- lib/arelastic/filters/geo_bounding_box.rb
+++ lib/arelastic/filters/geo_bounding_box.rb
@@ -2,19 +2,15 @@ module Arelastic
   module Filters
     class GeoBoundingBox < Arelastic::Filters::Filter
       attr_accessor :field, :top_left, :bottom_right, :options
-      def initialize(field, options)
+      def initialize(field, top_left, bottom_right, options = {})
         @field = field
-        options = options.dup
-        @top_left = options.delete(:top_left) || (raise "missing :top_left")
-        @bottom_right = options.delete(:bottom_right) || (raise "missing :bottom_right")
+        @top_left = top_left
+        @bottom_right = bottom_right
         @options = options
       end
 
       def as_elastic
-        params = options.update field => {
-          "top_left"     => top_left,
-          "bottom_right" => bottom_right
-        }
+        params = {field => {"top_left" => top_left, "bottom_right" => bottom_right}}.update(options)
 
         { "geo_bounding_box" => params }
       end
