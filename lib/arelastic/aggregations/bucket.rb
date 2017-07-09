eb8607cc45442fa0d73533575fcca2a82871ccf6
Matthew Higgins <developer@matthewhiggins.com>
2017-07-09 15:51:00 -0700

    Add bucket_test

--- lib/arelastic/aggregations/bucket.rb
+++ lib/arelastic/aggregations/bucket.rb
@@ -4,8 +4,9 @@ module Arelastic
       attr_accessor :aggs
 
       def initialize(name, options = {})
+        options = options.dup
+        @aggs = options.delete(:aggs) || options.delete('aggs')
         super(name, options)
-        @aggs = options[:aggs] || options['aggs']
       end
 
       def as_elastic
