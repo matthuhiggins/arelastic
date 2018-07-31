94389e2238d60ad09f75b0b0ce56ca4fb8d28413
Dan Cromer <cromer.dan@gmail.com>
2018-07-31 15:37:20 -0700

    Percolate query supports multiple documents

--- test/arelastic/queries/percolate_test.rb
+++ test/arelastic/queries/percolate_test.rb
@@ -12,6 +12,20 @@ class Arelastic::Queries::PercolateTest < Minitest::Test
     assert_equal expected, percolate.as_elastic
   end
 
+  def test_as_elastic_with_multiple_documents
+    percolate = Arelastic::Queries::Percolate.new(
+      "query",
+      [{ "color" => "red" }, { "color" => "blue" }]
+    )
+    expected = {
+      "percolate" => {
+        "field" => "query",
+        "documents" => [{ "color" => "red" }, { "color" => "blue" }]
+      }
+    }
+    assert_equal expected, percolate.as_elastic
+  end
+
   def test_as_elastic_with_options
     percolate = Arelastic::Queries::Percolate.new("query", {"color" => "red"}, {"document_type" => "widget"})
     expected = {
