229c079cd749dc7ee2a020d2f38ddf1812b4e4eb
Eric Barendt <ericb@gotime.com>
2018-11-28 14:39:16 -0800

    Add helpers for the rest of the queries

--- lib/arelastic/builders/query.rb
+++ lib/arelastic/builders/query.rb
@@ -2,18 +2,36 @@ module Arelastic
   module Builders
     class Query < Struct.new :name
       MACROS_TO_ARELASTIC = {
-        bool:           Arelastic::Queries::Bool,
-        constant_score: Arelastic::Queries::ConstantScore,
-        ids:            Arelastic::Queries::Ids,
-        field:          Arelastic::Queries::Field,
-        match:          Arelastic::Queries::Match,
-        match_all:      Arelastic::Queries::MatchAll,
-        match_none:     Arelastic::Queries::MatchNone,
-        match_phrase:   Arelastic::Queries::MatchPhrase,
-        multi_match:    Arelastic::Queries::MultiMatch,
-        prefix:         Arelastic::Queries::Prefix,
-        term:           Arelastic::Queries::Term,
-        terms:          Arelastic::Queries::Terms
+        bool:                Arelastic::Queries::Bool,
+        constant_score:      Arelastic::Queries::ConstantScore,
+        dis_max:             Arelastic::Queries::DisMax,
+        exists:              Arelastic::Queries::Exists,
+        field:               Arelastic::Queries::Field,
+        filter:              Arelastic::Queries::Filter,
+        function_score:      Arelastic::Queries::FunctionScore,
+        fuzzy:               Arelastic::Queries::Fuzzy,
+        geo_bounding_box:    Arelastic::Queries::GeoBoundingBox,
+        geo_distance:        Arelastic::Queries::GeoDistance,
+        has_child:           Arelastic::Queries::HasChild,
+        ids:                 Arelastic::Queries::Ids,
+        limit:               Arelastic::Queries::Limit,
+        match:               Arelastic::Queries::Match,
+        match_all:           Arelastic::Queries::MatchAll,
+        match_none:          Arelastic::Queries::MatchNone,
+        match_phrase:        Arelastic::Queries::MatchPhrase,
+        multi_match:         Arelastic::Queries::MultiMatch,
+        nested:              Arelastic::Queries::Nested,
+        percolate:           Arelastic::Queries::Percolate,
+        prefix:              Arelastic::Queries::Prefix,
+        query:               Arelastic::Queries::Query,
+        query_string:        Arelastic::Queries::QueryString,
+        range:               Arelastic::Queries::Range,
+        regexp:              Arelastic::Queries::Regexp,
+        script:              Arelastic::Queries::Script,
+        simple_query_string: Arelastic::Queries::SimpleQueryString,
+        term:                Arelastic::Queries::Term,
+        terms:               Arelastic::Queries::Terms,
+        wildcard:            Arelastic::Queries::Wildcard
       }
 
       class << self
