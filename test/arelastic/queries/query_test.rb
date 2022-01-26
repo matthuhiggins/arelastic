57e421ed5d97dfa91b9f81acd6837224ee0f48f0
Isaac Betesh <isaac.betesh@data-axle.com>
2022-01-26 13:00:41 -0500

    Add shortcut methods `has_child` and `has_parent`

--- test/arelastic/queries/query_test.rb
+++ test/arelastic/queries/query_test.rb
@@ -16,6 +16,36 @@ class Arelastic::Queries::QueryTest < Minitest::Test
     assert_equal(expected, nested_query.as_elastic)
   end
 
+  def test_has_child
+    query = Arelastic::Queries::Term.new('foo', 'bar')
+
+    nested_query = query.has_child 'links'
+
+    assert nested_query.is_a?(Arelastic::Queries::HasChild)
+    expected = {
+      "has_child" => {
+        "type" => "links",
+        "query" => query.as_elastic
+      }
+    }
+    assert_equal(expected, nested_query.as_elastic)
+  end
+
+  def test_has_parent
+    query = Arelastic::Queries::Term.new('foo', 'bar')
+
+    nested_query = query.has_parent 'links'
+
+    assert nested_query.is_a?(Arelastic::Queries::HasParent)
+    expected = {
+      "has_parent" => {
+        "parent_type" => "links",
+        "query" => query.as_elastic
+      }
+    }
+    assert_equal(expected, nested_query.as_elastic)
+  end
+
   def test_negate
     filter = Arelastic::Queries::Term.new 'foo', 'bar'
 
