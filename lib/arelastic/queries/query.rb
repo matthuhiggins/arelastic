ab067c7234bc7b8ee1b920a749edb045484f7afe
Isaac Betesh <77296306+betesh-at-data-axle@users.noreply.github.com>
2022-01-26 13:05:58 -0500

    Add shortcut methods `has_child` and `has_parent` (#33)

--- lib/arelastic/queries/query.rb
+++ lib/arelastic/queries/query.rb
@@ -5,6 +5,14 @@ module Arelastic
         Arelastic::Queries::Nested.new path, self
       end
 
+      def has_child path
+        Arelastic::Queries::HasChild.new path, self
+      end
+
+      def has_parent path
+        Arelastic::Queries::HasParent.new path, self
+      end
+
       def negate
         Arelastic::Queries::Bool.new must_not: self
       end
