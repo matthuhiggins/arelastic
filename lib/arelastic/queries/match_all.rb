c52ca2e0106fc0760c218abd741e18c7923b55ac
Isaac Betesh <isaac.betesh@data-axle.com>
2022-05-04 16:59:12 -0400

    Don't duplicate `MatchAll`'s

--- lib/arelastic/queries/match_all.rb
+++ lib/arelastic/queries/match_all.rb
@@ -4,6 +4,14 @@ module Arelastic
       def as_elastic
         {"match_all" => {}}
       end
+
+      def hash
+        0
+      end
+
+      def eql?(other)
+        other.is_a?(self.class)
+      end
     end
   end
 end
